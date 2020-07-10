91a0767c1d30b1a860f153399b2cd29f8dd72112
StefanGolubovic <stefan.golubovic3@gmail.com>
2020-07-10 10:02:14 +0200

    clicking game

---
+++ ClickingGameNode/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+/* GET home page. */
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'clickGameDB', collectionName = 'score';
+let db = null;
+let collection = null;
+// Use connect method to connect to the server
+MongoClient.connect(url,  { useUnifiedTopology: true }, function(err, client) {
+  if(err) throw err;
+  console.log("Connected successfully to server");
+
+  db = client.db(dbName);
+  collection = db.collection(collectionName);
+  //client.close();
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/addScore', function (req, res) {
+
+  collection.insertOne(req.body, function (err, result) {
+    if(err) throw err;
+    console.log("Inserted!");
+    res.send("OK");
+  });
+
+});
+
+router.post('/getScores', async function (req, res) {
+
+  let scores = await collection.find().toArray();
+  res.send(scores);
+});
+
+module.exports = router;
---
+++ ClickingGameNode/views/main.js
@@ -0,0 +1,70 @@
+var totalScore = 0;
+var timer;
+var seconds=59;
+var timerStarted = false;
+function clickedFunc(clicked_id) {
+
+    if(!timerStarted) {
+        timer = window.setInterval(function() { 
+          myFunction();
+        }, 1000);
+    }
+    timerStarted = true;
+    var numClicked = clicked_id;
+    totalScore += 1;
+    var color = document.getElementById(clicked_id).style.backgroundColor;
+    if (color === "lightslategray") {
+        document.getElementById(clicked_id).style.backgroundColor = "white";
+       printIt();
+    }
+    else {
+        window.alert("Oops...\nTry again");
+        location.reload();
+        
+    }
+    document.getElementById("scoreHeader").innerHTML = "Score: " + totalScore;
+}
+
+async function myFunction() {
+    if(seconds < 60) {
+      document.getElementById("timerHeader").innerHTML = seconds + " ";
+    }
+    if (seconds >0 ) {
+       seconds--;
+    } else {
+       clearInterval(timer);
+        let res = await axios.post('/addScore', {"score": totalScore, "time": new Date()});
+       window.alert("Your total score is: " + totalScore);
+       console.log(res);
+    }
+}
+
+(function mainFunction() {
+
+    initialize();
+})();
+
+function printIt() {
+    var printed = false;
+    var rand = parseInt(16 * Math.random()) + 1;
+    while(!printed) {
+        if(document.getElementById(rand).style.backgroundColor == "lightslategray"){
+            rand = parseInt(16 * Math.random()) + 1;
+        }
+        else{
+            document.getElementById(rand).style.backgroundColor = "lightslategray";
+            printed = true;
+        }
+    }
+}
+
+function initialize() {
+    
+    for (i = 0; i < 5 ; i++) {
+        printIt();
+    }
+}
+
+function goToScore() {
+    window.location.href='http://localhost:3000';
+}
